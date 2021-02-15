1e5423d250452135f2f8c9391b23f38d2997c686
Rachael Blake <rblake@rstudio03.research.sesync.org>
2021-02-15 11:12:14 -0500

    remove duplicate X axis labels in Fig1

--- Lep_analyses_R-code_reduced.R
+++ Lep_analyses_R-code_reduced.R
@@ -93,7 +93,7 @@ mod_native3 <- lm(native3$value_log ~ native3$area_log, data=native3)
 
 
 # Function to create scatterplots  
-make_scatterplots <- function(df, title){
+make_scatterplots <- function(df, title, xlab){
   model <- lm(df$value_log ~ df$area_log, data = df)
   
   p <- ggplot(df, aes_string(x = df$area, y = df$value)) +
@@ -101,7 +101,8 @@ make_scatterplots <- function(df, title){
     geom_text_repel(label = df$name) +  # from the package ggrepel; puts the text in good places
     geom_smooth(method = lm, se = FALSE) + 
     theme_cowplot() + 
-    xlab(expression(paste("Area (km" ^ "2", ")"))) + ylab("No. Species") +
+    xlab(xlab) + # xlab(expression(paste("Area (km" ^ "2", ")"))) + 
+    ylab("No. Species") +
     scale_x_log10() + 
     scale_y_log10(breaks = c(1, 10, 100, 1000, 10000, 100000)) +
     labs(title = title)
@@ -119,12 +120,12 @@ make_scatterplots <- function(df, title){
 }
 
 
-a1 <- make_scatterplots(alien1, "b)") 
-a2 <- make_scatterplots(alien2, "d)")
-a3 <- make_scatterplots(alien3, "f)")
-n1 <- make_scatterplots(native1, "a)")
-n2 <- make_scatterplots(native2, "c)")
-n3 <- make_scatterplots(native3, "e)")
+a1 <- make_scatterplots(alien1, "b)", NULL) 
+a2 <- make_scatterplots(alien2, "d)", NULL)
+a3 <- make_scatterplots(alien3, "f)", expression(paste("Area (km" ^ "2", ")")))
+n1 <- make_scatterplots(native1, "a)", NULL)
+n2 <- make_scatterplots(native2, "c)", NULL)
+n3 <- make_scatterplots(native3, "e)", expression(paste("Area (km" ^ "2", ")")))
 
 Fig1 <- plot_grid(n1, a1, n2, a2, n3, a3, ncol = 2)
 

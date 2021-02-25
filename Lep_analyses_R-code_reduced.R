6a4656c56acd5d2aa5d4c2af1bd978bd5584c8b2
Rachael Blake <rblake@rstudio03.research.sesync.org>
2021-02-25 17:02:33 -0500

    remove Fig2 axis tick labels

--- Lep_analyses_R-code_reduced.R
+++ Lep_analyses_R-code_reduced.R
@@ -86,21 +86,28 @@ mod_native3 <- lm(native3$value_log ~ native3$area_log, data=native3)
 
 
 # Function to create scatterplots  
-make_scatterplots <- function(df, title, xlab){
-  model <- lm(df$value_log ~ df$area_log, data = df)
+make_scatterplots <- function(df, title, xlab, xticklab){
+                     model <- lm(df$value_log ~ df$area_log, data = df)
   
-  p <- ggplot(df, aes_string(x = df$area, y = df$value)) +
-    geom_point() +
-    geom_text_repel(label = df$name) +  # from the package ggrepel; puts the text in good places
-    geom_smooth(method = lm, se = FALSE) + 
-    theme_cowplot() + 
-    xlab(xlab) + # xlab(expression(paste("Area (km" ^ "2", ")"))) + 
-    ylab("No. Species") +
-    scale_x_log10() + 
-    scale_y_log10(breaks = c(1, 10, 100, 1000, 10000, 100000)) +
-    labs(title = title)
+                     p <- ggplot(df, aes_string(x = df$area, y = df$value)) +
+                          geom_point() +
+                          geom_text_repel(label = df$name) +  # from the package ggrepel; puts the text in good places
+                          geom_smooth(method = lm, se = FALSE) + 
+                          theme_cowplot() + 
+                          xlab(xlab) + # xlab(expression(paste("Area (km" ^ "2", ")"))) + 
+                          ylab("No. Species") +
+                          scale_x_log10() + 
+                          scale_y_log10(breaks = c(1, 10, 100, 1000, 10000, 100000)) +
+                          labs(title = title) 
   
-  p <- ggdraw(p) + 
+                     if(xticklab == "labels"){ p1 <- p
+                        }else{
+                          if(xticklab == element_blank()){
+                             p1 <- p + theme(axis.text.x = xticklab)
+                             }
+                          }
+  
+  p2 <- ggdraw(p1) + 
     draw_label(label = paste0("R-squared = ", round(summary(model)$r.squared, 3)), 
                x = 0.85, y = 0.2, size = 12, fontface = "italic") +
     draw_label(label = paste0("Slope = ", round(summary(model)$coefficients[[2]], 6)), 
@@ -108,17 +115,18 @@ make_scatterplots <- function(df, title, xlab){
   
   print(paste0("plot_", deparse(substitute(df))))
   
-  return(p)
+  return(p2)
   
 }
 
+# This code prints errors, but the plots look correct, you it appears you can ignore the errors.
+a1 <- make_scatterplots(alien1, "b)", NULL, element_blank()) 
+a2 <- make_scatterplots(alien2, "d)", NULL, element_blank())
+a3 <- make_scatterplots(alien3, "f)", expression(paste("Area (km" ^ "2", ")")), "labels")
+n1 <- make_scatterplots(native1, "a)", NULL, element_blank())
+n2 <- make_scatterplots(native2, "c)", NULL, element_blank())
+n3 <- make_scatterplots(native3, "e)", expression(paste("Area (km" ^ "2", ")")), "labels")
 
-a1 <- make_scatterplots(alien1, "b)", NULL) 
-a2 <- make_scatterplots(alien2, "d)", NULL)
-a3 <- make_scatterplots(alien3, "f)", expression(paste("Area (km" ^ "2", ")")))
-n1 <- make_scatterplots(native1, "a)", NULL)
-n2 <- make_scatterplots(native2, "c)", NULL)
-n3 <- make_scatterplots(native3, "e)", expression(paste("Area (km" ^ "2", ")")))
 
 Fig2 <- plot_grid(n1, a1, n2, a2, n3, a3, ncol = 2)
 
